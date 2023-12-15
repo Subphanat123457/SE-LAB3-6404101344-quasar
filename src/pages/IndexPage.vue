16cf665eee489c85aae5fa2ad1ff8717fd72b50a
MFL <irpg17102545@gmail.com>
2023-12-15 10:47:00 +0700

    Changed name from english to thai language

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -8,8 +8,8 @@
       <q-input
         filled
         v-model="name"
-        label="Your name *"
-        hint="Name and surname"
+        label="ชื่อของคุณ *"
+        hint="ชื่อ และ นามสกุล"
         lazy-rules
         :rules="[ val => val && val.length > 0 || 'Please type something']"
       />
